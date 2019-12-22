d84bcaa578403d48729a2afc799f471f60d760d7
rhyanz46 <rianariansaputra@gmail.com>
2019-12-22 16:26:24 +0800

    update install script

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ echo 'DATABASE_HOST="your_localhost"
 DATABASE_USER="your_user"
 DATABASE_PASSWORD="your_pass"
 DATABASE_NAME="your_dbname"' > .env
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask new
