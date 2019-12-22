192d8f229bd36a2534e4849ab8bf7703f326c210
rhyanz46 <rianariansaputra@gmail.com>
2019-12-22 16:56:32 +0800

    fixing create_supervisor_conf.py

--- create_supervisor_conf.py
+++ create_supervisor_conf.py
@@ -8,6 +8,6 @@ config = '[program:be_sangkareang]' \
          '\nautorestart=true\n'.format(os.getcwd(), os.getcwd())
 
 if os.path.isdir(folder_supervisor):
-    f = open(folder_supervisor + "/be_sangkareang.conf", "a")
+    f = open(folder_supervisor + "/be_sangkareang.conf", "w")
     f.write(config)
     f.close()
